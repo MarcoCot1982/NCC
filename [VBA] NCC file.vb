5a6d9481b717c4b4091c43c823e4273e85c8a58d
Marco Cot <118482073+MarcoCot1982@users.noreply.github.com>
2022-11-21 17:07:02 +0000

    Update [VBA] NCC file.vb

--- [VBA] NCC file.vb
+++ [VBA] NCC file.vb
@@ -4,89 +4,83 @@
 '*
 '* ACCOUNT: GLOBAL
 '* Standardize data imput for NCC tickets
-'* 
 '*
-Sub Agent(Optional HiddenMacro as String)
+'*
+Sub Agent(Optional HideMe As String)
 '
 ' Agent Macro
 '
 
 '
+On Error GoTo ErrorControl
 'Desprotege hoja 2 y hoja 1
     Application.ScreenUpdating = False
     Sheets("LIST").Select
-    ActiveSheet.Unprotect
+    ActiveSheet.Unprotect "NoEdit"
     Sheets("Agent").Select
-    ActiveSheet.Unprotect
+    ActiveSheet.Unprotect "NoEdit"
+    Range("2:2").Font.Color = vbBlack
 'Verifica input de Account, Channel y Action (check empty)
-        If Range("C2") = "" Then
-        Range("C2").Select
-        Range("C2").Interior.COLOR = vbYellow
-        ActiveSheet.PROTECT DrawingObjects:=True, Contents:=True, Scenarios:=True
-        Sheets("LIST").Select
-        ActiveSheet.PROTECT DrawingObjects:=True, Contents:=True, Scenarios:=True
-        Sheets("Agent").Select
-        Range("C2").Select
-    ElseIf Range("D2") = "" Then
-        Range("C2").Select
-        Range("C2").Interior.COLOR = xlNone
-        Range("D2").Select
-        Range("D2").Interior.COLOR = vbYellow
-        ActiveSheet.PROTECT DrawingObjects:=True, Contents:=True, Scenarios:=True
-        Sheets("LIST").Select
-        ActiveSheet.PROTECT DrawingObjects:=True, Contents:=True, Scenarios:=True
-        Sheets("Agent").Select
-        Range("C2").Select
-    ElseIf Range("E2") = "" Then
-        Range("D2").Select
-        Range("D2").Interior.COLOR = xlNone
-        Range("E2").Select
-        Range("E2").Interior.COLOR = vbYellow
-        ActiveSheet.PROTECT DrawingObjects:=True, Contents:=True, Scenarios:=True
-        Sheets("LIST").Select
-        ActiveSheet.PROTECT DrawingObjects:=True, Contents:=True, Scenarios:=True
-        Sheets("Agent").Select
-        Range("C2").Select
-    ElseIf Range("F2") = "" Then
-        Range("E2").Select
-        Range("E2").Interior.COLOR = xlNone
-        Range("F2").Select
-        Range("F2").Interior.COLOR = vbYellow
-        ActiveSheet.PROTECT DrawingObjects:=True, Contents:=True, Scenarios:=True
-        Sheets("LIST").Select
-        ActiveSheet.PROTECT DrawingObjects:=True, Contents:=True, Scenarios:=True
-        Sheets("Agent").Select
-        Range("C2").Select
+    If Range("SERVICE") = "" Then
+            Range("SERVICE").Interior.Color = vbYellow
+            ActiveSheet.PROTECT "NoEdit"
+            Sheets("LIST").Select
+            ActiveSheet.PROTECT "NoEdit"
+            Sheets("Agent").Select
+            Range("SERVICE").Select
+            MsgBox "Enter service name", vbExclamation + vbOKOnly, "MISSING DATA"
+        ElseIf Range("TICKET") = "" Then
+            Range("SERVICE").Interior.Color = xlNone
+            Range("TICKET").Interior.Color = vbYellow
+            ActiveSheet.PROTECT "NoEdit"
+            Sheets("LIST").Select
+            ActiveSheet.PROTECT "NoEdit"
+            Sheets("Agent").Select
+            Range("TICKET").Select
+            MsgBox "Enter ticket number", vbExclamation + vbOKOnly, "MISSING DATA"
+        ElseIf Range("CONTACT") = "" Then
+            Range("SERVICE").Interior.Color = xlNone
+            Range("TICKET").Interior.Color = xlNone
+            Range("CONTACT").Interior.Color = vbYellow
+            ActiveSheet.PROTECT "NoEdit"
+            Sheets("LIST").Select
+            ActiveSheet.PROTECT "NoEdit"
+            Sheets("Agent").Select
+            Range("CONTACT").Select
+            MsgBox "Enter contact type", vbExclamation + vbOKOnly, "MISSING DATA"
+        ElseIf Range("ACTION") = "" Then
+            Range("SERVICE").Interior.Color = xlNone
+            Range("TICKET").Interior.Color = xlNone
+            Range("CONTACT").Interior.Color = xlNone
+            Range("ACTION").Interior.Color = vbYellow
+            ActiveSheet.PROTECT "NoEdit"
+            Sheets("LIST").Select
+            ActiveSheet.PROTECT "NoEdit"
+            Sheets("Agent").Select
+            Range("ACTION").Select
+            MsgBox "Enter action performed", vbExclamation + vbOKOnly, "MISSING DATA"
 'Verifica input de Account, Channel y Action (check allowed text)
     ElseIf Range("D3") = 0 Then
-        Range("C2:F2").Select
-        Range("C2:F2").Interior.COLOR = vbRed
-        Range("G2").Select
-        ActiveCell.FormulaR1C1 = _
-        "DATOS NO PERMITIDOS"
-        ActiveSheet.PROTECT DrawingObjects:=True, Contents:=True, Scenarios:=True
+        ActiveSheet.PROTECT "NoEdit"
         Sheets("LIST").Select
-        ActiveSheet.PROTECT DrawingObjects:=True, Contents:=True, Scenarios:=True
+        ActiveSheet.PROTECT "NoEdit"
         Sheets("Agent").Select
-        Range("C2").Select
+        Range("SERVICE").Select
+        MsgBox "Datos no permitidos", vbCritical + vbOKOnly, "ERROR"
     Else
-    Range("C2").Select
-    Range("C2:E2").Interior.COLOR = xlNone
+        Range("SERVICE").Select
+        Range("SERVICE:CONTACT").Interior.Color = xlNone
 'Agrega Fecha
-    Range("A2").Select
-    ActiveCell.FormulaR1C1 = "=TODAY()"
-    Range("F2:G2").Select
+    Range("A2") = Date
+    Range("ACTION:G2").Select
     Selection.Replace What:="", Replacement:="N/A", LookAt:=xlPart, _
-        SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False, FormulaVersion:=xlReplaceFormula2
-    Selection.Replace What:="DATOS NO PERMITIDOS", Replacement:="N/A", LookAt:=xlPart, _
         SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
         ReplaceFormat:=False, FormulaVersion:=xlReplaceFormula2
 'Copia/Pega en el listado
     Range("2:2").Select
     Selection.Copy
     Sheets("LIST").Select
-    ActiveSheet.Unprotect
+    ActiveSheet.Unprotect "NoEdit"
     Range("2:2").Select
     Selection.Insert Shift:=xlDown
     Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks _
@@ -102,7 +96,7 @@ Sub Agent(Optional HiddenMacro as String)
 'Volver a proteger y restaurar formato tabla
     Selection.Locked = True
     Selection.FormulaHidden = False
-    ActiveSheet.PROTECT DrawingObjects:=True, Contents:=True, Scenarios:=True
+    ActiveSheet.PROTECT "NoEdit"
     Range("A1").Select
     Sheets("Agent").Select
     Range("A2:G2").Select
@@ -144,13 +138,16 @@ Sub Agent(Optional HiddenMacro as String)
     End With
     Range("C2:G2").Select
     Selection.Locked = False
-    Range("B3").Select
+    Range("STOREDNAME").Select
     Selection.Copy
-    Range("B2").Select
+    Range("NAME").Select
     Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks _
         :=False, Transpose:=False
-    ActiveSheet.PROTECT DrawingObjects:=True, Contents:=True, Scenarios:=True
-    Range("C2").Select
+    ActiveSheet.PROTECT "NoEdit"
+    Range("SERVICE").Select
     Application.ScreenUpdating = True
     End If
+
+    ErrorControl:
+        MsgBox "An error occurred." & vbCrLf & "Code: " & Err.Number & vbCrLf & "Description: " & Err.Description, vbInformation, "ERROR"
 End Sub
