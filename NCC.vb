471cf85351a59e5ed4f9d5b593d835e3c2611b8e
Marco Cot <118482073+MarcoCot1982@users.noreply.github.com>
2022-12-05 23:35:00 +0000

    Update NCC.vb

--- NCC.vb
+++ NCC.vb
@@ -72,6 +72,8 @@ Sub Agent(Optional HideMe As String)
         Range("SERVICE:CONTACT").Interior.Color = xlNone
 'Agrega Fecha
     Range("A2") = Date
+    Range("ACTION:G2").Select
+    'Cells.Replace "", "N/A", xlWhole [DEPRECATED]
 'Copia/Pega en el listado
     Range("2:2").Select
     Selection.Copy
@@ -154,9 +156,86 @@ Sub Agent(Optional HideMe As String)
 
 End Sub
 
+--------------------------------------------
 
-Private Sub Workbook_SheetSelectionChange(ByVal Sh As Object, ByVal Target As Range)
-    On Error Resume Next
-    Target.PasteSpecial xlPasteValues
-    Application.CutCopyMode = True
+WORKBOOK EVENT
+
+Private Sub Workbook_BeforeClose(Cancel As Boolean)
+
+    Sheets("LIST").Protect "NoEdit"
+    Sheets("AGENT").Protect "NoEdit"
+
 End Sub
+
+Private Sub Workbook_Open()
+
+    Dim Services As Integer
+    Dim Contacts As Integer
+    Dim Actions As Integer
+    
+    On Error GoTo ErrorShortCut
+    
+    Application.ScreenUpdating = False
+    Worksheets("AGENT").Unprotect "NoEdit"
+    Worksheets("IMPORT").Visible = xlSheetVisible
+    Sheets("IMPORT").Select
+    Range("F25").Select
+    Selection.ListObject.QueryTable.Refresh BackgroundQuery:=False
+    Range("H6:J55").Copy
+    Sheets("Agent").Select
+    Range("H6:J55").Select
+    ActiveSheet.Paste
+        
+    Services = Worksheets("Agent").Range("H7:H57").Cells.SpecialCells(xlCellTypeConstants).Count + 6
+    Contacts = Worksheets("Agent").Range("I7:I57").Cells.SpecialCells(xlCellTypeConstants).Count + 6
+    Actions = Worksheets("Agent").Range("J7:J57").Cells.SpecialCells(xlCellTypeConstants).Count + 6
+    Range("Tabla1[[#Headers],[SERVICE]]").Select
+    Range(Selection, Selection.End(xlDown)).Select
+    ActiveWorkbook.Names.Add Name:="SERVICELIST", RefersToR1C1:= _
+        "=Agent!R6C8:R" & Services & "C8"
+    Range("Tabla1[[#Headers],[TYPE OF CONTACT]]").Select
+    Range(Selection, Selection.End(xlDown)).Select
+    ActiveWorkbook.Names.Add Name:="CONTACTLIST", RefersToR1C1:= _
+        "=Agent!R6C9:R" & Contacts & "C9"
+    Range("Tabla1[[#Headers],[ACTION]]").Select
+    Range(Selection, Selection.End(xlDown)).Select
+    ActiveWorkbook.Names.Add Name:="ACTIONLIST", RefersToR1C1:= _
+        "=Agent!R6C10:R" & Actions & "C10"
+    
+    Worksheets("RESTORE").Visible = xlSheetVisible
+    Sheets("RESTORE").Select
+    Range("C2:G3").Copy
+    Sheets("AGENT").Select
+    Range("C2:G3").Select
+    ActiveSheet.Paste
+    Range("SERVICE").Select
+    ActiveCell.Formula2R1C1 = _
+        "=INDEX(LIST!RC:R[98]C,MODE(MATCH(LIST!RC:R[98]C,LIST!RC:R[98]C,0)))"
+    Range("SERVICE").Copy
+     Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks _
+        :=False, Transpose:=False
+        
+    ActiveWorkbook.Names.Add Name:="SERVICE", RefersToR1C1:= _
+        "=Agent!R2C3"
+    ActiveWorkbook.Names.Add Name:="TICKET", RefersToR1C1:= _
+        "=Agent!R2C4"
+    ActiveWorkbook.Names.Add Name:="CONTACT", RefersToR1C1:= _
+        "=Agent!R2C5"
+    ActiveWorkbook.Names.Add Name:="ACTION", RefersToR1C1:= _
+        "=Agent!R2C6"
+        
+    Worksheets("AGENT").Protect "NoEdit"
+    Worksheets("IMPORT").Visible = xlSheetHidden
+    Worksheets("RESTORE").Visible = xlSheetHidden
+    
+    Application.ScreenUpdating = True
+    Application.CutCopyMode = False
+
+ErrorShortCut:
+    Worksheets("AGENT").Protect "NoEdit"
+    Worksheets("IMPORT").Visible = xlSheetHidden
+    Worksheets("RESTORE").Visible = xlSheetHidden
+    Application.ScreenUpdating = True
+End Sub
+
+
